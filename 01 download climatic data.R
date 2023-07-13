aa67f556ccdadc1fab098f6c7c43e3fa5fec540f
fabiolexcastro <fabioalexandercastro@gmail.com>
2023-07-13 17:57:52 -0500

    download

---
+++ 01 download climatic data.R
@@ -0,0 +1,15 @@
+
+# Load libraries ----------------------------------------------------------
+install.packages('pacman') # Correr una única vez
+require(pacman)
+pacman::p_load(terra, sf, fs, tidyverse, RColorBrewer, ggspatial, geodata, glue)
+
+g <- gc(reset = T)
+rm(list = ls())
+options(scipen = 999, warn = -1)
+
+# Load data ---------------------------------------------------------------
+zone <- terra::vect('shp/base/buffer30.shp')
+
+
+
